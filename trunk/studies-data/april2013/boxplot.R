049ba664dce6256714ce04cfab640ba74f4809f7
manuel.palomo@gm.uca.es <manuel.palomo@gm.uca.es@b0694143-2b53-21ad-dca1-81ded6413a41>
2013-04-07 16:53:12 +0000

    Cambio para coherencia

--- trunk/studies-data/april2013/boxplot.R
+++ trunk/studies-data/april2013/boxplot.R
@@ -1,4 +1,4 @@
-cmp <- read.csv("april2013.csv", sep=";", dec=".")
+cmp <- read.csv("pre-post-tests.csv", sep=";", dec=".")
 boxplot(lex ~ TEST, data = cmp, ylab = "Lexical Accuracy" )
 boxplot(gram ~ TEST, data = cmp, ylab = "Grammatical accuracy")
 boxplot(writing ~ TEST, data = cmp, ylab = "Writing skills")
